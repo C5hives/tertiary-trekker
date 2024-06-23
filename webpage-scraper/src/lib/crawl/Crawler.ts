b4092a0c7d5dc427057f4accdf8e53406aaf49cf
your name <kokbochang@gmail.com>
2024-06-23 15:51:59 +0800

    change cluster object in crawler to an instance variable

--- webpage-scraper/src/lib/crawl/Crawler.ts
+++ webpage-scraper/src/lib/crawl/Crawler.ts
@@ -15,7 +15,7 @@ import HtmlParser from '../parse/HtmlParser';
 import UrlFilter from '../parse/UrlFilter';
 
 class Crawler {
-    private static cluster: Cluster;
+    private cluster!: Cluster;
 
     private urlFilter: UrlFilter;
     private tracker: CrawlTracker;
@@ -26,9 +26,6 @@ class Crawler {
     private savePath: string;
     private universityName: string; // currently very hacky
 
-    /**
-     * Main constructor.
-     */
     public constructor(savePath: string, urlPatterns: string[], tracker: CrawlTracker, universityName: string) {
         this.savePath = savePath;
         this.urlFilter = new UrlFilter();
@@ -41,13 +38,6 @@ class Crawler {
         this.universityName = universityName;
     }
 
-    /**
-     * Scrapes the specified urls and saves their html content to local files.
-     *
-     * @param savePath - The root directory to save the files to
-     * @param options - The urls to include and exclude when web crawling
-     * @returns A void Promise
-     */
     public async scrapeAll(urls: string[]): Promise<void> {
         if (urls.length < 1) {
             appLogger.debug('No urls provided to crawl');
@@ -75,22 +65,27 @@ class Crawler {
         return;
     }
 
-    private async markCrawledUrlsAsVisited(): Promise<void> {
-        const date: string = JobDate.getCurrentDateString();
-        // convert map into an array of objects
-        const urls: { url: string; outcome: string }[] = Array.from(this.visitedUrls, ([url, outcome]) => {
-            return {
-                url: url,
-                outcome: outcome
-            };
-        });
-        await this.tracker.include.markUrlsAsVisited(urls, date);
+    private async initialiseBlacklistedUrls(): Promise<void> {
+        const excludedUrls: string[] = await this.tracker.exclude.getBlacklistedUrls(this.universityName);
+        this.urlFilter.setBlacklistedUrls(excludedUrls);
         return;
     }
 
-    private async addNewUrlsToCrawl(): Promise<void> {
-        const urls: string[] = Array.from(this.discoveredUrls.values());
-        await this.tracker.include.insertUrlsToVisit(urls);
+    private async initialiseCluster(): Promise<void> {
+        this.cluster = await Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_CONTEXT,
+            maxConcurrency: 5,
+            puppeteerOptions: { headless: true },
+            retryLimit: 2,
+            sameDomainDelay: 5000,
+            skipDuplicateUrls: true,
+            timeout: 30000,
+            monitor: false,
+            workerCreationDelay: 100,
+            puppeteer: puppeteer.use(StealthPlugin())
+        });
+        // set function that each worker will run
+        await this.cluster.task(this.crawlUrl);
         return;
     }
 
@@ -142,13 +137,6 @@ class Crawler {
         return;
     }
 
-    /**
-     * Returns the html contents of a website as a string.
-     *
-     * @param page - A Page object used to visit website urls
-     * @param url - The url to scrape data from
-     * @returns A Promise containing the contents of the website
-     */
     private async getContentFromUrl(page: Page, url: string): Promise<{ content: string; newUrl: string }> {
         // Navigate the page to a URL
         const response: HTTPResponse | null = await page.goto(url, { waitUntil: 'domcontentloaded' });
@@ -160,6 +148,41 @@ class Crawler {
         const content = await page.content();
         const newUrl = page.url();
         return { content, newUrl };
+    }
+
+    private queueUrls(urls: string[]): void {
+        for (const url of urls) {
+            this.cluster.queue({
+                url: url,
+                crawler: this
+            });
+        }
+        return;
+    }
+
+    private async waitForClusterToFinishCrawl(): Promise<void> {
+        await this.cluster.idle();
+        await this.cluster.close();
+        return;
+    }
+
+    private async markCrawledUrlsAsVisited(): Promise<void> {
+        const date: string = JobDate.getCurrentDateString();
+        // convert map into an array of objects
+        const urls: { url: string; outcome: string }[] = Array.from(this.visitedUrls, ([url, outcome]) => {
+            return {
+                url: url,
+                outcome: outcome
+            };
+        });
+        await this.tracker.include.markUrlsAsVisited(urls, date);
+        return;
+    }
+
+    private async addNewUrlsToCrawl(): Promise<void> {
+        const urls: string[] = Array.from(this.discoveredUrls.values());
+        await this.tracker.include.insertUrlsToVisit(urls);
+        return;
     }
 
     private shouldCrawl(url: string): boolean {
@@ -180,46 +203,6 @@ class Crawler {
         }
 
         return true;
-    }
-
-    private async initialiseCluster(): Promise<void> {
-        Crawler.cluster = await Cluster.launch({
-            concurrency: Cluster.CONCURRENCY_CONTEXT,
-            maxConcurrency: 5,
-            puppeteerOptions: { headless: true },
-            retryLimit: 2,
-            sameDomainDelay: 5000,
-            skipDuplicateUrls: true,
-            timeout: 30000,
-            monitor: false,
-            workerCreationDelay: 100,
-            puppeteer: puppeteer.use(StealthPlugin())
-        });
-        // set function that each worker will run
-        await Crawler.cluster.task(this.crawlUrl);
-        return;
-    }
-
-    private async waitForClusterToFinishCrawl(): Promise<void> {
-        await Crawler.cluster.idle();
-        await Crawler.cluster.close();
-        return;
-    }
-
-    private async initialiseBlacklistedUrls(): Promise<void> {
-        const excludedUrls: string[] = await this.tracker.exclude.getBlacklistedUrls(this.universityName);
-        this.urlFilter.setBlacklistedUrls(excludedUrls);
-        return;
-    }
-
-    private queueUrls(urls: string[]): void {
-        for (const url of urls) {
-            Crawler.cluster.queue({
-                url: url,
-                crawler: this
-            });
-        }
-        return;
     }
 }
 
